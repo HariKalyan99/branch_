bce8a1b1bff70e07008b4d91430380843a52092d
Harikalyan <harikalyan99tronics@gmail.com>
2024-10-07 07:48:51 +0530

    logic added

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 function binarySearch(A, X){
-    return `Find the target ${X} position in the array: ${A}`
+    let s = 0;
+    let e = A.length - 1;
+
+    while(s <= e){
+        let mid = Math.floor((s+e) / 2);
+
+        if(A[mid] < X){
+            s = mid + 1;
+
+        }else if(A[mid] > X){
+            e = mid - 1;
+        }else {
+            return mid;
+        }
+    }
+
+    return -1;
 }
+
 console.log(binarySearch([1,2,4,5,8], 5))
